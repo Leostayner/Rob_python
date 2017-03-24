f319f1496b907c09ad95ab2863a17fd5c2980dcb
Leostayner <leostayner@gmail.com>
2017-03-23 22:33:14 -0300

    bump funcionando

--- scripts/untitled.py
+++ scripts/untitled.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
+#imports
 import rospy
 import numpy as np
 import tf
@@ -14,22 +14,37 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import Header
 from ar_track_alvar_msgs.msg import AlvarMarker, AlvarMarkers
+from neato_node.msg import Bump
+from sensor_msgs.msg import LaserScan
 
 bridge = CvBridge()
 
-known_width = 6.5 #Diameter in cm of the circunference
-wpix = 116 #Inicial diameter in cm of the circunference
-d = 34 #Inicial dist from the camera to the circunference in cm
+#Variaveis Para Calcular a Distancia do objeto
+known_width = 6.5
+wpix = 116 
+d = 34 
 focal_dist = (wpix * d) / known_width
 
+z = 100
+z_desejado = 80
+
+#variaveis do scaner_1
+Frente_es = 0
+Lado_es = 0
+Frente_dir = 0 
+Lado_dir = 0
+
+#Variavies de detecção do objeto
 cv_image = None
 media = []
 centro = [320, 240]
 atraso = 1.5
-vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-z = 100
-z_desejado = 80
 
+#variavel de velocidade
+vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
+
+#processamento da imagem
 def processa(dado):
 	global media
 	global centro
@@ -43,17 +58,19 @@ def processa(dado):
 	contornos, arvore = cv2.findContours(bordas, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)	
 	maior_contorno = None
 	maior_contorno_area = 0
+	
+
 	# imagem de saida
 	disp = cv2.cvtColor(frame_rg, cv2.COLOR_GRAY2BGR)
     	for cnt in contornos:
     	    area = cv2.contourArea(cnt)
-	    
-z = (known_width * focal_dist) / (2 * cnt[2])
+	    #z = (known_width * focal_dist) / (2 * cnt[2])
     	    if area > maior_contorno_area:
     	        maior_contorno = cnt
     	        maior_contorno_area = area
     
-    	# Encontramos o centro do contorno fazendo a média de todos seus pontos.
+    	
+	# Encontramos o centro do contorno fazendo a média de todos seus pontos.
     	if not maior_contorno is None :
     	    cv2.drawContours(frame, [maior_contorno], -1, [0, 0, 255], 5)
     	    maior_contorno = np.reshape(maior_contorno, (maior_contorno.shape[0], 2))
@@ -66,6 +83,7 @@ z = (known_width * focal_dist) / (2 * cnt[2])
 	cv2.imshow('caixa', disp)
 
 	cv2.waitKey(1)
+
 
 def recebe(imagem):
 	global cv_image
@@ -87,6 +105,22 @@ def recebe(imagem):
 	except CvBridgeError as e:
 		print(e)
 
+
+#Função do Scaner Bump
+def recebe_bump(msg):
+	global Frente_es
+	print("foiiiiiii")
+	global Lado_es
+	global Frente_dir
+	global Lado_dir
+
+	Frente_es = msg.leftFront
+	Lado_es = msg.leftSide
+	Frente_dir = msg.rightFront
+	Lado_dir = msg.rightSide
+
+
+#Função para movimentação do robo
 def Movimento(dif_x):
 	global media
 	print(dif_x)
@@ -111,16 +145,26 @@ def Movimento(dif_x):
 		return  Twist(Vector3(0.0,0,0), Vector3(0,0,0))
 			
 
+# Função para procurar o objeto
 def Procura():
 	print('Procurando')
 	return Twist(Vector3(0,0,0), Vector3(0,0,-0.2))
 	
+#def recebe_scan(msg):
+#	global v_scan
+#		for i in msg.renges:
+#			if i < 300:
+#	v_scan = laser.ranges.index(i)
+#	print(v_scan)
+
 
 if __name__=="__main__":
 
 	rospy.init_node("cor")
 	recebedor = rospy.Subscriber("/camera/image_raw", Image, recebe, queue_size=10, buff_size = 2**24)
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+	recebedor_bumper = rospy.Subscriber('/bump', Bump, recebe_bump)
+#	recebedor_scan = rospy.Subscriber('/scan' , LaserScan, recebe_scan)
 
 	cv2.namedWindow("caixa")
 	cv2.namedWindow("video")
@@ -128,17 +172,29 @@ if __name__=="__main__":
 	try:
 
 		while not rospy.is_shutdown():
+			SL = 0.1
+			if (Frente_es or Lado_es) == 1:
+				vel = Twist(Vector3(-0.8, 0, 0), Vector3(0,0, -0.8))		
+				SL = 0.8
+			elif (Frente_dir or Lado_dir) == 1:
+				vel = Twist(Vector3(-0.8, 0, 0), Vector3(0,0, 0.8))
+				SL = 0.8
+			elif (Frente_dir and Frente_es) == 1:
+				vel = Twist(Vector3(-0.5, 0, 0), Vector3(0,0,1))
+				SL = 0.8
+				print("Saiiii")
+			
 
-			if len(media) != 0 and len(centro) != 0:
+			elif len(media) != 0 and len(centro) != 0:
 				dif_x = media[0]-centro[0]
 				dif_y = media[1]-centro[1]
 				vel = Movimento(dif_x)
 			else:
 				vel = Procura()
-
-			velocidade_saida.publish(vel)
 			print(vel)
-			rospy.sleep(0.1)
+			velocidade_saida.publish(vel)
+			
+			rospy.sleep(SL)
 
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
